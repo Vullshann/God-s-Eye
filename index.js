109dd10e3cfe58fe6166df1b83219de0fa54d1b1
louminia1 <39954825+louminia1@users.noreply.github.com>
2019-06-27 00:10:48 +0200

    bot

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const Discord = require("discord.js")
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone : true});
 const conf = require("./config.json")
 
+bot.on("ready", async () => {
 
+    let statues = [
+        `serveur : ${bot.guilds.size}`,
+        `channel : ${bot.channels.size}`,
+        `version : V1.0`,
+        `membre : ${bot.users.size}`
+    ]
+    setInterval(() => {
+        let status = statues[Math.floor(Math.random() * statues.length)]
+        bot.user.setActivity(status, {type: "WATCHING"});
+    }, 9000);
+    console.log("Bot connected...")
+    console.log("___________________")
+    bot.user.setStatus("Online")
+});
 
 
 
